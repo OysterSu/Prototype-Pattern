1a8161eba8ff8f5c742fcd980ec0d3d8b66f2d0a
蘇健豪 <jenhausu@icloud.com>
2020-01-02 17:48:00 +0800

    Initial Commit

---
+++ PrototypePattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrototypePattern
+//
+//  Created by 蘇健豪1 on 2020/1/2.
+//  Copyright © 2020 Oyster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
