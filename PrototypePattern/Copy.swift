2e6fb3b41687dc9aabe0f6a7b7dc9d6089d1adb7
蘇健豪 <jenhausu@icloud.com>
2020-01-02 17:49:28 +0800

    init Copy protocol

---
+++ PrototypePattern/Copy.swift
@@ -0,0 +1,23 @@
+//
+//  Copy.swift
+//  PrototypePattern
+//
+//  Created by 蘇健豪1 on 2020/1/2.
+//  Copyright © 2020 Oyster. All rights reserved.
+//
+
+import Foundation
+
+protocol Copying: class {
+  
+    init(_ prototype: Self)
+    
+}
+
+extension Copying {
+    
+    func copy() -> Self {
+        return type(of: self).init(self)
+    }
+    
+}
