fc9c113fc24eb85d9168950b57aa0bfb11b79d40
蘇健豪 <jenhausu@icloud.com>
2020-01-02 18:13:27 +0800

    make Demo code better

--- PrototypePattern/ViewController.swift
+++ PrototypePattern/ViewController.swift
@@ -15,11 +15,17 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         let monster = Monster(health: 700, level: 37)
-        let monster2 = monster.copy()
-        monster2.health = 800
+        print(monster.health)
         
+        let monster2 = monster
+        monster2.health = 800
         print(monster.health)
         print(monster2.health)
+        
+        let monster3 = monster.copy()
+        monster3.health = 900
+        print(monster.health)
+        print(monster3.health)
     }
     
 }
