56ddddbd2749350cb74601ca5227d714f2c4e6a5
蘇健豪 <jenhausu@icloud.com>
2020-01-05 03:46:43 +0800

    MessageLogger class

---
+++ PrototypePattern/MessagLogger.swift
@@ -0,0 +1,23 @@
+//
+//  MessagLogger.swift
+//  PrototypePattern
+//
+//  Created by 蘇健豪1 on 2020/1/5.
+//  Copyright © 2020 Oyster. All rights reserved.
+//
+
+import Foundation
+
+class MessageLogger {
+    var messages: [Message] = []
+
+    func logMessage(msg: Message) {
+        messages.append(msg)
+    }
+
+    func proccessMessages(callback: @escaping (Message) -> Void ) {
+        for msg in messages {
+            callback(msg)
+        }
+    }
+}
